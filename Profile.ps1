862f4cfba7f7372ef647ee02415968496c715056
Sean Francis N. Ballais <sean@seanballais.com>
2024-05-29 13:11:08 +0800

    Update Profile.ps1 to reflect the latest changes.

--- Profile.ps1
+++ Profile.ps1
@@ -27,6 +27,35 @@ if ($Env:SFB_ENABLE_VS_DEV_BANNER -eq "true") {
 
 # Allows us to use tools installed via Visual Studio.
 Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell 46f3bcbf -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64 $no_banner_param"
+Enter-VsDevShell a009adc0 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64 $no_banner_param"
 
-oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "C:\Users\sean\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json" | Invoke-Expression
+
+# Set up pipenv prompt.
+# Based on: https://github.com/pypa/pipenv/issues/4264#issuecomment-845445399
+if ($env:PIPENV_ACTIVE -eq 1) {
+    function _OLD_PROMPT { "" }
+    Copy-Item -Path function:prompt -Destination function:_OLD_PROMPT
+
+    $_PROMPT_PREFIX = (($env:VIRTUAL_ENV -split "\\")[-1] -split "-")[0]
+    $pipenv_name = ((($(pip -V) -split ' ')[3]) -split '\\')[4]
+
+    function prompt {
+        Write-Host -NoNewline -ForegroundColor White "($_PROMPT_PREFIX) " 
+        _OLD_PROMPT
+    
+    }
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Set up rbenv.
+& "$Env:RBENV_ROOT\rbenv\bin\rbenv.ps1" init
